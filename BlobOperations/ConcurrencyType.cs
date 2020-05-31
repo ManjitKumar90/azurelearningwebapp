360b2096e21208aec2039c4b5410e9b351d5698c
Smruti Ranjan Panda <smpanda@microsoft.com>
2020-05-31 20:33:15 +0530

    Added SQL APi Queries

---
+++ BlobOperations/ConcurrencyType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlobOperations
+{
+    public enum ConcurrencyType
+    {
+        Default,
+        Optimistic,
+        Pessimistic
+    }
+}
--- BlobOperations/Program.cs
+++ BlobOperations/Program.cs
@@ -42,10 +42,67 @@ namespace BlobOperations
 
             await blockBlob.SetMetadataAsync();
 
-            await blockBlob.DownloadToFileAsync("copyof" + ImageToUplaod, System.IO.FileMode.Create);
+            //await HandleConCurrencyForUpdate(blockBlob, ConcurrencyType.Default);
+            //await HandleConCurrencyForUpdate(blockBlob, ConcurrencyType.Optimistic);
+            await HandleConCurrencyForUpdate(blockBlob, ConcurrencyType.Pessimistic);
 
-            await blockBlob.DeleteAsync();
+            //await blockBlob.DownloadToFileAsync("copyof" + ImageToUplaod, System.IO.FileMode.Create);
 
+            //await blockBlob.DeleteAsync();
+
+        }
+
+        private static async Task<bool> HandleConCurrencyForUpdate(CloudBlockBlob blockBlob, ConcurrencyType type)
+        {
+            bool result = false;
+            switch(type)
+            {
+                case ConcurrencyType.Default:
+                    blockBlob.Metadata["Author"] = "Default Update";
+                    blockBlob.Metadata["Priority"] = "High";
+                    await blockBlob.SetMetadataAsync();
+                    break;
+                case ConcurrencyType.Optimistic:
+                    blockBlob.Metadata["Author"] = "Optimistic Update";
+                    blockBlob.Metadata["Priority"] = "High";
+                    var accessCondition = new AccessCondition
+                    {
+                        IfMatchETag = blockBlob.Properties.ETag
+                    };
+                    try
+                    {
+                        await blockBlob.SetMetadataAsync(accessCondition, null, null);
+                    }
+                    catch(Exception exe)
+                    {
+                        result = false;
+                    }
+                    
+                    break;
+                case ConcurrencyType.Pessimistic:
+                    blockBlob.Metadata["Author"] = "Optimistic Update";
+                    blockBlob.Metadata["Priority"] = "High";
+                    string leaseId = await blockBlob.AcquireLeaseAsync(TimeSpan.FromMinutes(1));
+                    var accessConditionPessimistic = new AccessCondition
+                    {
+                        LeaseId = leaseId
+                    };
+                    try
+                    {
+                        await blockBlob.SetMetadataAsync(accessConditionPessimistic, null, null);
+                    }
+                    catch(Exception exe)
+                    {
+                        result = false;
+                    }
+
+                    //await blockBlob.BreakLeaseAsync(TimeSpan.FromSeconds(0));
+
+                    await blockBlob.ReleaseLeaseAsync(accessConditionPessimistic);
+                    break;
+            }
+
+            return result;
         }
     }
 }
