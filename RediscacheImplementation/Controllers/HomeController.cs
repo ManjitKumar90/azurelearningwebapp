35a4c7105953f506b662684bab849cc59451a7e1
Smruti Ranjan Panda <smpanda@microsoft.com>
2020-06-21 20:11:09 +0530

    Added Redis Storage

---
+++ RediscacheImplementation/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using RediscacheImplementation.Models;
+using RediscacheImplementation.Services;
+
+namespace RediscacheImplementation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly ICacheService cacheService;
+        public HomeController(ILogger<HomeController> logger, ICacheService cacheService)
+        {
+            _logger = logger;
+            this.cacheService = cacheService;
+        }
+
+        public IActionResult Index()
+        {
+            Employee employee =  cacheService.GetData<Employee>("employeeId").GetAwaiter().GetResult();
+
+            if(employee == null)
+            {
+                return Content("Cache Has Not been Set");
+            }
+
+            return Content(JsonConvert.SerializeObject(employee));
+            
+        }
+
+        public IActionResult SetCache()
+        {
+            var employee = new Employee()
+            {
+                Id = "123456",
+                Name = "Rohit"
+            };
+            cacheService.SetData<Employee>("employeeId", employee, TimeSpan.FromMinutes(3));
+            return Content("Cache Has been Set");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RediscacheImplementation/Services/CacheService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace RediscacheImplementation.Services
+{
+    public class CacheService : ICacheService
+    {
+        private readonly IDatabase database;
+
+        private readonly IConfiguration configuration;
+
+        public CacheService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            string connectionString = this.configuration["RedisConnectionString"];
+            database = GetDataBase(connectionString);
+        }
+
+        private IDatabase GetDataBase(string connectionString)
+        {
+            var lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+            {
+
+                return ConnectionMultiplexer.Connect(connectionString);
+            });
+
+            return lazyConnection.Value.GetDatabase();
+        }
+
+        public async Task<T> GetData<T>(string key)
+        {
+            key = key ?? throw new System.ArgumentNullException(nameof(key));
+
+            var result =  await database.StringGetAsync(key).ConfigureAwait(false);
+
+            return (result == RedisValue.Null) ? default(T) : JsonConvert.DeserializeObject<T>(result);
+        }
+
+        public async Task SetData<T>(string key, T t, TimeSpan timeSpan)
+        {
+            var json = JsonConvert.SerializeObject(t);
+            await database.StringSetAsync(key, json, timeSpan);
+        }
+    }
+}
