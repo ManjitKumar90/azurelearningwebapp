ec58ff4900547f901d5d7eee0b91fa8928e7305c
Smruti Ranjan Panda <smpanda@microsoft.com>
2020-06-28 23:01:16 +0530

    Application Insights Chnages

--- AppInsightWebApp/Controllers/HomeController.cs
+++ AppInsightWebApp/Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using AppInsightWebApp.Models;
+using System.Net.Http;
+using Microsoft.ApplicationInsights;
+using System.Threading;
 
 namespace AppInsightWebApp.Controllers
 {
@@ -15,16 +18,41 @@ namespace AppInsightWebApp.Controllers
 
         public HomeController(ILogger<HomeController> logger)
         {
+
             _logger = logger;
         }
 
         public IActionResult Index()
         {
+            TelemetryClient client = new TelemetryClient();
+            client.TrackTrace("HomePage", new Dictionary<string, string>()
+            {
+                ["RequestId"] = Guid.NewGuid().ToString(),
+                ["Message"] = "Home Controller Is Called"
+
+            }); 
+            CallGithub().Wait();
             return View();
+        }
+
+        private async Task CallGithub()
+        {
+            HttpClient client = new HttpClient();
+            int exp = 2;
+            int count = 1;
+            do
+            {
+                Thread.Sleep(count * 1000);
+                await client.GetAsync("https://api.github.com/users").ConfigureAwait(false);
+                count = count * exp;
+            }
+            while (count < 16);
+            
         }
 
         public IActionResult Privacy()
         {
+            throw new InvalidOperationException("Something Went Wrong");
             return View();
         }
 
